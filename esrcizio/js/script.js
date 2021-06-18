c97bd0238bbf5c3e7743b8d992186ef205cd4d95
parix1999 <parisottoedoardo@gmail.com>
2021-06-18 15:57:40 +0200

    esercizio(1)

--- esrcizio/js/script.js
+++ esrcizio/js/script.js
@@ -22,10 +22,12 @@ var LapiùLeggera;
 
 for (var x = 0; x < bici.length; x++) {
     var weight = bici[x].peso;
-    
+    var n = bici[x].nome;
     if (typeof LapiùLeggera === 'undefined' || weight < LapiùLeggera) {
         biciLeggera = bici[x];
         LapiùLeggera = weight;   
     }
 }
+console.log(n)
 console.log(LapiùLeggera)
+document.getElementById('result').innerHTML = `La Bici con il peso minore è: ${n} con un peso di: ${LapiùLeggera} `;
